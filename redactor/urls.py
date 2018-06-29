4c7ce72d7053f185f622cf814434ae3352b26ce0
HenrikOssipoff <henrik.ossipoff@gmail.com>
2018-06-29 10:31:59 +0200

    Make Django 2.0 compatible

--- redactor/urls.py
+++ redactor/urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls.defaults import url, patterns
+from django.urls import re_path
 
 from redactor.views import redactor_upload
 from redactor.forms import FileForm, ImageForm
 
 
-urlpatterns = patterns('',
-    url('^upload/image/(?P<upload_to>.*)', redactor_upload, {
+urlpatterns = [
+    re_path('^upload/image/(?P<upload_to>.*)', redactor_upload, {
         'form_class': ImageForm,
         'response': lambda name, url: '<img src="%s" alt="%s" />' % (url, name),
     }, name='redactor_upload_image'),
 
-    url('^upload/file/(?P<upload_to>.*)', redactor_upload, {
+    re_path('^upload/file/(?P<upload_to>.*)', redactor_upload, {
         'form_class': FileForm,
         'response': lambda name, url: '<a href="%s">%s</a>' % (url, name),
     }, name='redactor_upload_file'),
-)
+]
